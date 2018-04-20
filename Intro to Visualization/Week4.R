48de25b3914750aa21eb75b6e5a308c41b7a6de2
SY Young <r38411@yahoo.com>
2018-04-20 20:39:00 +0800

    more classnote

--- Intro to Visualization/Week4.R
+++ Intro to Visualization/Week4.R
@@ -194,3 +194,22 @@ ex_comp_dist4 <- function() {
     #p <- p + facet_grid(year~.)
     p
 }
+
+ex_density1 <- function() {
+    past_year <- 1970
+    present_year <- 2010
+    gapminder <- gapminder %>% mutate(dollars_per_day=gdp/population/365)
+    country_list_1 <- gapminder %>% filter(year==past_year & !is.na(dollars_per_day)) %>% .$country
+    country_list_2 <- gapminder %>% filter(year==present_year & !is.na(dollars_per_day)) %>% .$country
+    country_list <- intersect(country_list_1, country_list_2)
+    west <- c("Western Europe", "Northern Europe", "Southern Europe")
+    west <- c(west, "Northern America", "Australia and New Zealand")
+    
+    s <- gapminder %>% filter(year %in% c(past_year,present_year) & country %in% country_list)
+    s <- mutate(s, group=ifelse(region%in%west, "West", "Developing"))
+    p <- ggplot(s, aes(dollars_per_day, y= ..count.., fill=group)) 
+    p <- p + scale_x_continuous(trans="log2")
+    p <- p + geom_density(alpha=0.2) + facet_grid(year~.)
+    
+    p
+}
