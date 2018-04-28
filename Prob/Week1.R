c819054187b5b22af57eb95849d2202bcd8ad6e7
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-04-28 18:08:14 +0800

    sec1.2

--- Prob/Week1.R
+++ Prob/Week1.R
@@ -10,3 +10,66 @@ fun1 <- function() {
   events <- sample(beads, B, replace=TRUE)
   prop.table(table(events))
 }
+
+loadWeek1 <- function() {
+  library(gtools)
+}
+
+permu1 <- function() {
+  suits <- c("Diamonds","Clubs","Hearts","Spades")
+  numbers <-c("Ace","Deuce","Three","Four","Five",
+              "Six","Seven","Eight","Nine","Ten",
+              "Jack","Queen","King")
+  deck <- expand.grid(number=numbers,suit=suits)
+  deck <- paste(deck$number,deck$suit)
+  
+  # calculate prob of a king
+  kings <- paste("King",suits)
+  mean(deck %in% kings)
+  
+  # now draw 2 card
+  hands <- permutations(52, 2, v=deck)
+  first_card <- hands[,1]
+  second_card <- hands[,2]
+  sum(first_card %in% kings & second_card %in% kings)/
+      sum(first_card %in% kings)
+}
+
+comb_phone_num <- function() {
+  all_phone_numbers <- permutations(10,7,v=0:9)
+  n <- nrow(all_phone_numbers)
+  index <- sample(n,5)
+  all_phone_numbers[index,]
+}
+
+create_deck <- function() {
+  suits <- c("Diamonds","Clubs","Hearts","Spades")
+  numbers <-c("Ace","Deuce","Three","Four","Five",
+              "Six","Seven","Eight","Nine","Ten",
+              "Jack","Queen","King")  
+}
+
+combine1 <- function() {
+  create_deck()
+  deck <- expand.grid(number=numbers,suit=suits)
+  # cal the prob of blackjack
+  aces <- paste("Ace",suits)
+  facecard <- c("King","Queen","Jack","Ten")
+  facecard <- expand.grid(number=facecard,suit=suits)
+  
+  hands <- combinations(52,2,v=deck)
+  mean(hands[,1] %in% aces & hands[,2] %in% facecard)
+  
+}
+
+monte21 <- function() {
+  create_deck()
+  hand <- samples(deck, 2)
+  B <- 10000
+  results <- replicate(B, {
+    hand <- sample(deck,2)
+    (hand[1] %in% aces & hand[2] %in% facecard) |
+      (hand[2] %in% aces & hand[1] %in% facecard)
+  })
+  mean(results)
+}
