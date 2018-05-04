078c4bca7dc55292bc3f189d82f35e5bcb76e888
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-05-04 19:25:22 +0800

    update week1 classnote

--- Prob/Week1.R
+++ Prob/Week1.R
@@ -124,3 +124,20 @@ whichEnough <- function() {
     prob <- sapply(B, compute_prob)
     plot(x=log10(B), y=prob, type="l")
 }
+
+monty_hall <- function() {
+  B <- 10000
+  stick <- replicate(B, {
+    doors <- as.character(1:3)
+    prize <- sample(c("car","goat","goat"))
+    prize_door <- doors[prize=="car"]
+    my_pick <- sample(doors, 1)
+    show <- sample(doors[!doors %in% c(my_pick,prize_door)],1)
+    stick <- my_pick
+    stick == prize_door
+    
+    switch <- doors[!doors %in%  c(my_pick,show)]
+    switch == prize_door
+  })
+  mean(stick)
+}
