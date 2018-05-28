3497589fb65caef1c6457329273d04fec1f60ad7
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-05-28 13:46:12 +0800

    week5

--- Inference/Week6.R
+++ Inference/Week6.R
@@ -63,3 +63,64 @@ elec_forc_model1 <- function() {
   
   1 - pnorm(0, posterior_mean, posterior_se)
 }
+
+pred_elec_college <- function() {
+  results_us_election_2016 %>% arrange(desc(electoral_votes)) 
+    %>% top_n(5, electoral_votes)
+  results <- polls_us_election_2016 %>%
+    filter(state!="U.S" & !grepl("CD",state) &
+             enddate >= "2016-10-31" &
+             (grade %in% c("A+","A","A-","B+") |
+                is.na(grade))) %>%
+    mutate(spread=rowpoll_clinton/100 = rawpoll_trump/100) %>%
+    group_by(state) %>%
+    summarize(avg=mean(spread),sd=sd(spread),n=n()) %>%
+    mutate(state = as.character(state))
+  
+  results %>% arrange(abs(avg))
+  results <- left_join(results, 
+                       results_us_election_2016, by="state")
+  results_us_election_2016 %>%
+    filter(!state %in% results$state)
+  results <- results %>%
+    mutate(sd=ifelse(is.na(sd), median(results$sd,na.rm=TRUE),sd))
+
+  # now use monte-carlo simulation to simulate result
+  mu <- 0
+  tau <- 0.02
+  results %>% mutate(sigma = sd/sqrt(n),
+                     B = sigma^2/(sigma^2 + tau^2),
+                     posterior_mean=B*mu+(1-B)*avg,
+                     posterior_se=sqrt(1/(1/sigma^2 + 1/tau^2))) %>%
+    arrange(abs(posterior_mean))
+  # now we generate 10000 times
+  mu <- 0
+  tau <- 0.02
+  clinton_EV <- replicate(1000, {
+    results %>% mutate(sigma = sd/sqrt(n),
+                       B = sigma^2/(sigma^2 + tau^2),
+                       posterior_mean=B*mu+(1-B)*avg,
+                       posterior_se=sqrt(1/(1/sigma^2 + 1/tau^2)),
+                       simulated_result=rnorm(length(posterior_mean),
+                                              posterior_mean,
+                                              posterior_se),
+                       clinton=ifelse(simulated_result>0, electoral_votes,0)) %>%
+              summarize(clinton=sum(clinton)) %>%
+              .$clinton  +7## 7 for Rhode Island and D.C.
+  })
+  # however, this data ignore the general bias
+  # now we add the general bias in the sd
+  bias_sd <- 0.03
+  clinton_EV_2 <- replicate(1000, {
+    results %>% mutate(sigma =sqrt(sd^2/n +bias_sd^2),
+                       B = sigma^2/(sigma^2 + tau^2),
+                       posterior_mean=B*mu+(1-B)*avg,
+                       posterior_se=sqrt(1/(1/sigma^2 + 1/tau^2)),
+                       simulated_result=rnorm(length(posterior_mean),
+                                              posterior_mean,
+                                              posterior_se),
+                       clinton=ifelse(simulated_result>0, electoral_votes,0)) %>%
+      summarize(clinton=sum(clinton)) %>%
+      .$clinton  +7## 7 for Rhode Island and D.C.
+  })  
+}
