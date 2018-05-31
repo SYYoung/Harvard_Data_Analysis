e0688fa06fca0868724c6236d40f247f61ee8a27
SY Young <r38411@yahoo.com>
2018-05-31 13:02:32 +0800

    more update on week7

--- Inference/Week7.R
+++ Inference/Week7.R
@@ -19,5 +19,43 @@ assoc_1 <- function() {
     colnames(tab) <- c("Guessed Before","Guessed After")
     tab
     fisher.test(tab, alternative="greater")
+    totals
+}
+
+chi_square <- function() {
+    totals <- assoc_1()
+    totals
+    funding_rate <- totals %>%
+        summarize(percent_total=
+                      (yes_men+yes_women)/
+                      (yes_men+no_men+yes_women+no_women)) %>%
+        .$percent_total
+    funding_rate
+    
+    # use chi-squared test to find out
+    two_by_two <- tibble(awarded=c("no","yes"),
+                         men=c(totals$no_men,totals$yes_men),
+                         women=c(totals$no_women,totals$yes_women))
+    two_by_two
+    # compared above table with the overate
+    tibble(awarded=c("no","yes"),
+           men=(totals$no_men + totals$yes_men) *
+               c(1-funding_rate, funding_rate),
+           women=(totals$no_women + totals$yes_women) *
+               c(1-funding_rate, funding_rate))
+    two_by_two %>%
+        select(-awarded) %>%
+        chisq.test()
+    # the odds = P(Y|X)/P(~Y|X), X=1 if male, Y=1 if funded
+    odd_men <- (two_by_two$men[2]/sum(two_by_two$men)) /
+        (two_by_two$men[1]/sum(two_by_two$men))
+    odd_women<- (two_by_two$women[2]/sum(two_by_two$women))/
+        (two_by_two$women[1]/sum(two_by_two$women))
+    
+    # small p-value does not imply big odd ratio
+    two_by_two %>%
+        select(-awarded) %>%
+        mutate(men=men*10, women=women*10) %>%
+        chisq.test()
     
 }
